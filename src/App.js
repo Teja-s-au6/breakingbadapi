3cca6606d4d3517c7d503d37d7082382a1e4d107
tejaattainu <tejspvg@gmail.com>
2020-07-23 21:53:32 +0530

    Breading Bad API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, Link } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Logo from './images/logo.png'
 import CharacterDetail from './pages/CharacterDetail';
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <img src={Logo} alt='logo' style={{marginLeft: 750, marginTop: 20}} />
+      <Link to='/'><h1>Home</h1></Link>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/character/:characterId" component={CharacterDetail} />
